48707eb16f58a969ddd3f0f55e2776ae7df6fca3
Beidi Chen <beidic@fb.com>
2024-04-26 19:20:04 -0700

    submit jobs

--- submitted/gettingsqueue.sh
+++ submitted/gettingsqueue.sh
@@ -1,1 +1,12 @@
-squeue -u beidic 
+conda create -n yangllm2 python=3.9 
+conda activate yangllm2 
+pwd 
+ls 
+git clone https://github.com/YangZhou08/transformersprofiling.git 
+pip install torch 
+ls 
+cd transformersprofiling 
+pip install -e . 
+pip install datasets 
+pip install accelerate 
+pip install SentencePiece 
