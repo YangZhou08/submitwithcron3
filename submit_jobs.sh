09cd2bf081bff5a7681aa449f72fb5e94dbef1de
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-04-26 20:19:05 -0400

    makingnew

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,9 +1,10 @@
 #use your todo folder below
 # cd /fsx-storygen/beidic/yang/submitwithcron/todo 
-cd /fsx-storygen/beidic/yang/transformersprofiling 
-git pull 
+# cd /fsx-storygen/beidic/yang/transformersprofiling 
+# git pull 
 
-cd /fsx-storygen/beidic/yang/submitwithcron2/todo 
+# cd /fsx-storygen/beidic/yang/submitwithcron2/todo 
+cd /private/home/beidic/yang/submitwithcron3/todo 
 
 git fetch 
 LOCAL=$(git rev-parse main) 
@@ -13,26 +14,26 @@ if [ $LOCAL != $REMOTE ]; then
     changesToPull=true
 fi
 if $changesToPull; then
-    echo "There are changes to pull $REMOTE" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+    echo "There are changes to pull $REMOTE" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 else
-    echo "No changes to pull." >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+    echo "No changes to pull." >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 fi 
 
 git pull 
 
-echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Submitting jobs" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
-echo -e "There are $(ls -1 *.sh | wc -l) .sh files to submit." >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Submitting jobs" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
+echo -e "There are $(ls -1 *.sh | wc -l) .sh files to submit." >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 for script in *.sh; do
    # /opt/slurm/bin/sbatch "$script" 
-   echo -e "Submitting $script" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+   echo -e "Submitting $script" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
    # job_id=$(/opt/slurm/bin/sbatch "$script" | awk '{print $4}') 
    job_id=$(/opt/slurm/bin/sbatch "$script") 
-   echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Job ID: $job_id" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+   echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Job ID: $job_id" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 done 
 
 output=$(/opt/slurm/bin/squeue -u beidic) 
-echo -e "$output" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
-echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Done submitting jobs\n" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+echo -e "$output" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
+echo -e "$(date "+%Y-%m-%d %H:%M:%S") - Done submitting jobs\n" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 # echo -e "/fsx-storygen/beidic/yang/model_checkpoints/largemodeltinyllama_bf2f352_223990/" >> /fsx-storygen/beidic/yang/submitwithcron/submitted/my_log_file.txt 
 # echo -e "$(ls /fsx-storygen/beidic/yang/model_checkpoints/largemodeltinyllama_bf2f352_223990/)" >> /fsx-storygen/beidic/yang/submitwithcron/submitted/my_log_file.txt 
 # echo -e "/fsx-storygen/beidic/yang/model_checkpoints/largemodeltinyllama_70a2287_928748/" >> /fsx-storygen/beidic/yang/submitwithcron/submitted/my_log_file.txt 
@@ -42,16 +43,16 @@ wait
 
 mv *.sh ../submitted 
 
-cd /fsx-storygen/beidic/yang/submitwithcron2/bash_tasks 
-echo -e "There are $(ls -1 *.sh | wc -l) .sh bash files to run" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+cd /private/home/beidic/yang/submitwithcron3/bash_tasks 
+echo -e "There are $(ls -1 *.sh | wc -l) .sh bash files to run" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 for script in *.sh; do 
-    echo -e "Running $script" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
-    bash -x "$script" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 2>&1 & 
+    echo -e "Running $script" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
+    bash -x "$script" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 2>&1 & 
 done 
 
 mv *.sh ../submitted 
 
-echo -e "attempting to move error files" >> /fsx-storygen/beidic/yang/submitwithcron2/submitted/my_log_file.txt 
+echo -e "attempting to move error files" >> /private/home/beidic/yang/submitwithcron3/submitted/my_log_file.txt 
 
 # mv /fsx-storygen/beidic/yang/log/log-1550792.err ../submitted 
 # mv /fsx-storygen/beidic/yang/log/log-1550792.out ../submitted 
