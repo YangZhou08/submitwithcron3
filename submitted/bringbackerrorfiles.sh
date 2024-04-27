3312faa241f0c20916cfde496ce9e4ba7a3c2b34
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-04-26 20:11:20 -0400

    newcommit

---
+++ submitted/bringbackerrorfiles.sh
@@ -0,0 +1,37 @@
+# mv /fsx-storygen/beidic/yang/log/log-1664324.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664324.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664323.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664323.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664322.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664322.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664321.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664321.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664320.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664320.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664319.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664319.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1664318.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1664318.out ../submitted 
+
+mv /fsx-storygen/beidic/yang/log/log-1932254.err ../submitted 
+mv /fsx-storygen/beidic/yang/log/log-1932254.out ../submitted 
+# /opt/slurm/bin/scancel 1825137 
+# /opt/slurm/bin/scancel 1839692 
+
+# mv /fsx-storygen/beidic/yang/log/log-1929996.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1929996.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1929997.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1929997.out ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-1929998.err ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-1929998.out ../submitted 
+
+mv /fsx-storygen/beidic/yang/log/log-1934370.err ../submitted 
+mv /fsx-storygen/beidic/yang/log/log-1934370.out ../submitted 
