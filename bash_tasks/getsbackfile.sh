4371733dc8ac997ace0c45324cf10ead9dc5de4b
Beidi Chen <beidic@fb.com>
2024-05-20 06:25:05 -0700

    submit jobs

--- bash_tasks/getsbackfile.sh
+++
@@ -1,8 +0,0 @@
-# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
-
-mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
-mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
-
-mv /private/home/beidic/yang/log/log-27879840.err ../submitted 
-mv /private/home/beidic/yang/log/log-27879840.out ../submitted 
--- submitted/getsbackfile.sh
+++ submitted/getsbackfile.sh
@@ -1,8 +1,8 @@
 # mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
 # mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-27879839.err ../submitted 
-mv /private/home/beidic/yang/log/log-27879839.out ../submitted 
+mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
+mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-27879838.err ../submitted 
-mv /private/home/beidic/yang/log/log-27879838.out ../submitted 
+mv /private/home/beidic/yang/log/log-27879840.err ../submitted 
+mv /private/home/beidic/yang/log/log-27879840.out ../submitted 
