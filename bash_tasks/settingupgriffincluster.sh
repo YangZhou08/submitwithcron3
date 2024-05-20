7f92db2995a1fad1230e85910a0d753e17c287f4
Beidi Chen <beidic@fb.com>
2024-05-19 22:45:48 -0700

    submit jobs

--- bash_tasks/settingupgriffincluster.sh
+++
@@ -1,9 +0,0 @@
-pwd 
-# git clone git@github.com:Infini-AI-Lab/GRIFFIN2.git 
-cd GRIFFIN2 
-source /private/home/beidic/.bashrc
-source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh
-conda env create -f griffin.yml 
-conda activate griffin 
-cd src 
-pip install -e . 
