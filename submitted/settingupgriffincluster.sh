cf7abecccbe197ecfa0aebadaaebbab502125bcb
Beidi Chen <beidic@fb.com>
2024-05-19 23:45:05 -0700

    submit jobs

--- submitted/settingupgriffincluster.sh
+++ submitted/settingupgriffincluster.sh
@@ -1,12 +1,13 @@
 pwd 
 ls 
 rm -rf GRIFFIN2 
-cd ~ 
+cd /private/home/beidic/yang/ 
+ls 
 pwd 
 git clone git@github.com:Infini-AI-Lab/GRIFFIN2.git 
 cd GRIFFIN2 
-source /private/home/beidic/.bashrc 
-source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh
+# source /private/home/beidic/.bashrc 
+# source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh 
 conda env create -f griffin.yml 
 conda activate griffin 
 cd src 
