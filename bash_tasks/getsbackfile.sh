715942ae3521110b615839f93ef27ccc149940a7
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-20 09:21:41 -0400

    newjob

---
+++ bash_tasks/getsbackfile.sh
@@ -0,0 +1,8 @@
+# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
+# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
+
+# mv /private/home/beidic/yang/log/log-27879839.err ../submitted 
+# mv /private/home/beidic/yang/log/log-27879839.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-27879840.err ../submitted 
+mv /private/home/beidic/yang/log/log-27879840.out ../submitted 
