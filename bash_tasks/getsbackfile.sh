a4e3a8402cb0e21f775d5f906bfcdeedf020dcad
Beidi Chen <beidic@fb.com>
2024-05-20 07:20:05 -0700

    submit jobs

--- bash_tasks/getsbackfile.sh
+++
@@ -1,8 +0,0 @@
-# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
-
-# mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
-
-mv /private/home/beidic/yang/log/log-27880592.err ../submitted 
-mv /private/home/beidic/yang/log/log-27880592.out ../submitted 
