516d21af4d8b35ee3654e3a66bcad503264359d6
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-05-20 01:08:48 -0400

    making

---
+++ bash_tasks/settingupgriffincluster.sh
@@ -0,0 +1,9 @@
+pwd 
+# git clone git@github.com:Infini-AI-Lab/GRIFFIN2.git 
+cd GRIFFIN2 
+source /private/home/beidic/.bashrc
+source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh
+conda env create -f griffin.yml 
+conda activate griffin 
+cd src 
+pip install -e . 
