a4e953eca37774a952cfacdc46d1a2a2cc2e1c3b
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-30 20:49:12 -0700

    newtask

---
+++ bash_tasks/reinstalltransformers.sh
@@ -0,0 +1,9 @@
+source /private/home/beidic/.bashrc 
+source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh 
+source activate base 
+conda activate base 
+conda activate griffin 
+pip uninstall transformers 
+pip install transformers 
+
+python -c "from transformers.cache_utils import Cache, DynamicCache, StaticCache, SinkCache" 
