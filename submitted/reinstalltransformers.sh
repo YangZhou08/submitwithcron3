b823841937065e1f91182a0cd2b3b4dfda3be7aa
Beidi Chen <beidic@fb.com>
2024-05-30 21:00:04 -0700

    submit jobs

--- submitted/reinstalltransformers.sh
+++ submitted/reinstalltransformers.sh
@@ -3,7 +3,7 @@ source /public/apps/anaconda3/2022.05/etc/profile.d/conda.sh
 source activate base 
 conda activate base 
 conda activate griffin 
-pip uninstall transformers 
-pip install transformers 
+# pip uninstall transformers 
+pip install --force-reinstall transformers 
 
 python -c "from transformers.cache_utils import Cache, DynamicCache, StaticCache, SinkCache" 
