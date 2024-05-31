4a557282dce7e886341b8a6c83b5f4a3512658a3
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-30 20:39:26 -0700

    bringback

---
+++ bash_tasks/getsbackfile.sh
@@ -0,0 +1,14 @@
+# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
+# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
+
+# mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
+# mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28462313.err ../submitted 
+mv /private/home/beidic/yang/log/log-28462313.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28462312.err ../submitted 
+mv /private/home/beidic/yang/log/log-28462312.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28462311.err ../submitted 
+mv /private/home/beidic/yang/log/log-28462311.out ../submitted 
--- submitted/getsbackfile.sh
+++
@@ -1,8 +0,0 @@
-# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
-
-# mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
-
-mv /private/home/beidic/yang/log/log-27923798.err ../submitted 
-mv /private/home/beidic/yang/log/log-27923798.out ../submitted 
