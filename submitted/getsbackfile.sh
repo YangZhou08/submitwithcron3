a6095e207143f745bea4c505ab4d5dbe85808a6a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-01 02:18:04 -0700

    bringbackfiles

--- submitted/getsbackfile.sh
+++ submitted/getsbackfile.sh
@@ -4,11 +4,11 @@
 # mv /private/home/beidic/yang/log/log-27880591.err ../submitted 
 # mv /private/home/beidic/yang/log/log-27880591.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-28462564.err ../submitted 
-mv /private/home/beidic/yang/log/log-28462564.out ../submitted 
+# mv /private/home/beidic/yang/log/log-28462564.err ../submitted 
+# mv /private/home/beidic/yang/log/log-28462564.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-25462565.err ../submitted 
-mv /private/home/beidic/yang/log/log-28462565.out ../submitted 
+# mv /private/home/beidic/yang/log/log-25462565.err ../submitted 
+# mv /private/home/beidic/yang/log/log-28462565.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-29462566.err ../submitted 
-mv /private/home/beidic/yang/log/log-28462566.out ../submitted 
+mv /private/home/beidic/yang/log/log-28496978.err ../submitted 
+mv /private/home/beidic/yang/log/log-28496978.out ../submitted 
