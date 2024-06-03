bb3e32a3bbfc0569fb9f2bb23424bd24e00bc88a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-03 16:26:46 -0700

    newjobs

--- submitted/getsbackfile.sh
+++ submitted/getsbackfile.sh
@@ -10,8 +10,8 @@ mv /private/home/beidic/yang/log/log-28548885.out ../submitted
 mv /private/home/beidic/yang/log/log-28549290.err ../submitted 
 mv /private/home/beidic/yang/log/log-28549290.out ../submitted 
 
-mv /private/home/beidic/yang/log/log-28545806.err ../submitted 
-mv /private/home/beidic/yang/log/log-28545806.out ../submitted 
+mv /private/home/beidic/yang/log/log-28549291.err ../submitted 
+mv /private/home/beidic/yang/log/log-28549291.out ../submitted 
 
 mv /private/home/beidic/yang/log/log-28545542.err ../submitted 
 mv /private/home/beidic/yang/log/log-28545542.out ../submitted 
