a062a5aedf5fc635ecbb651287236ff0acfbec30
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-03 07:52:53 -0700

    llama

--- todo/llama3_8b_cats.sh
+++ todo/llama3_8b_cats.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=24:00:00
 
 ## partition name
-#SBATCH --partition=devlab
+#SBATCH --partition=learnfair 
 ## number of nodes
 #SBATCH --nodes=1
 
@@ -32,7 +32,6 @@ conda activate base
 # mamba activate yangllm 
 # conda activate yangllm 
 conda activate griffin 
-pip install --force-reinstall transformers==4.38.1
 # cd /fsx-storygen/beidic/yang/transformersprofiling 
 cd /private/home/beidic/yang/GRIFFIN2 
 git checkout yangexp2 
@@ -61,6 +60,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 echo 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 | transformers-cli login 
 huggingface-cli login --token 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 
 
-accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B,griffin=False,check=False --tasks gsm8k --batch_size 1 
-# accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-7b-chat-hf,cats=True,check=False --tasks gsm8k --batch_size 1 
+accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B,cats=True,check=False --tasks gsm8k --batch_size 1 
 # accelerate launch main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=True,thresh=0.9 --tasks gsm8k --batch_size 1 --limit=128 
