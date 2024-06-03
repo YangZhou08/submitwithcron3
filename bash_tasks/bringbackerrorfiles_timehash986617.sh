4a1f4b62b3ac297abab82fb7aa4838b3d8282a96
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-03 16:26:30 -0700

    newjobs

---
+++ bash_tasks/bringbackerrorfiles_timehash986617.sh
@@ -0,0 +1,45 @@
+mv /private/home/beidic/yang/log/log-28549360.err ../submitted
+mv /private/home/beidic/yang/log/log-28549360.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549288.err ../submitted
+mv /private/home/beidic/yang/log/log-28549288.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28548885.err ../submitted
+mv /private/home/beidic/yang/log/log-28548885.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549290.err ../submitted
+mv /private/home/beidic/yang/log/log-28549290.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549291.err ../submitted
+mv /private/home/beidic/yang/log/log-28549291.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549292.err ../submitted
+mv /private/home/beidic/yang/log/log-28549292.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549364.err ../submitted
+mv /private/home/beidic/yang/log/log-28549364.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549363.err ../submitted
+mv /private/home/beidic/yang/log/log-28549363.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549362.err ../submitted
+mv /private/home/beidic/yang/log/log-28549362.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549361.err ../submitted
+mv /private/home/beidic/yang/log/log-28549361.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28548889.err ../submitted
+mv /private/home/beidic/yang/log/log-28548889.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28549289.err ../submitted
+mv /private/home/beidic/yang/log/log-28549289.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28548886.err ../submitted
+mv /private/home/beidic/yang/log/log-28548886.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28548887.err ../submitted
+mv /private/home/beidic/yang/log/log-28548887.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28548888.err ../submitted
+mv /private/home/beidic/yang/log/log-28548888.out ../submitted
+
--- submitted/getsbackfile.sh
+++ submitted/getsbackfile.sh
@@ -1,17 +1,17 @@
-# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
-# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
+mv /private/home/beidic/yang/log/log-28549360.err ../submitted 
+mv /private/home/beidic/yang/log/log-28549360.out ../submitted 
 
-# mv /private/home/beidic/yang/log/log-28545620.err ../submitted 
-# mv /private/home/beidic/yang/log/log-28545620.out ../submitted 
+mv /private/home/beidic/yang/log/log-28549288.err ../submitted 
+mv /private/home/beidic/yang/log/log-28549288.out ../submitted 
 
-# mv /private/home/beidic/yang/log/log-28545665.err ../submitted 
-# mv /private/home/beidic/yang/log/log-28545665.out ../submitted 
+mv /private/home/beidic/yang/log/log-28548885.err ../submitted 
+mv /private/home/beidic/yang/log/log-28548885.out ../submitted 
 
-# mv /private/home/beidic/yang/log/log-28545805.err ../submitted 
-# mv /private/home/beidic/yang/log/log-28545805.out ../submitted 
+mv /private/home/beidic/yang/log/log-28549290.err ../submitted 
+mv /private/home/beidic/yang/log/log-28549290.out ../submitted 
 
-# mv /private/home/beidic/yang/log/log-28545806.err ../submitted 
-# mv /private/home/beidic/yang/log/log-28545806.out ../submitted 
+mv /private/home/beidic/yang/log/log-28545806.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545806.out ../submitted 
 
 mv /private/home/beidic/yang/log/log-28545542.err ../submitted 
 mv /private/home/beidic/yang/log/log-28545542.out ../submitted 
