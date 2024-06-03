a72487c664d8e5bc0d1d0e1e645c96add51dc350
Beidi Chen <beidic@fb.com>
2024-06-03 07:20:05 -0700

    submit jobs

---
+++ submitted/getsbackfile.sh
@@ -0,0 +1,17 @@
+# mv /private/home/beidic/yang/log/log-27875283.err ../submitted 
+# mv /private/home/beidic/yang/log/log-27875283.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28545620.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545620.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28545665.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545665.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28545805.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545805.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28545806.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545806.out ../submitted 
+
+mv /private/home/beidic/yang/log/log-28545620.err ../submitted 
+mv /private/home/beidic/yang/log/log-28545620.out ../submitted 
