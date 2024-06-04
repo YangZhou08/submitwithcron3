c3c33df64dc742f1c87e301c3910f2f981dd65fc
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-03 18:31:37 -0700

    newjob'

---
+++ bash_tasks/bringbackerrorfiles_timehash727587.sh
@@ -0,0 +1,12 @@
+mv /private/home/beidic/yang/log/log-28562611.err ../submitted
+mv /private/home/beidic/yang/log/log-28562611.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562681.err ../submitted
+mv /private/home/beidic/yang/log/log-28562681.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562609.err ../submitted
+mv /private/home/beidic/yang/log/log-28562609.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562682.err ../submitted
+mv /private/home/beidic/yang/log/log-28562682.out ../submitted
+
