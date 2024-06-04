c57e8240bc4c13afcfbb381183bdfa232c966372
Beidi Chen <beidic@fb.com>
2024-06-03 18:15:05 -0700

    submit jobs

---
+++ submitted/bringbackerrorfiles_timehash380538.sh
@@ -0,0 +1,15 @@
+mv /private/home/beidic/yang/log/log-28562611.err ../submitted
+mv /private/home/beidic/yang/log/log-28562611.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562610.err ../submitted
+mv /private/home/beidic/yang/log/log-28562610.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562609.err ../submitted
+mv /private/home/beidic/yang/log/log-28562609.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562608.err ../submitted
+mv /private/home/beidic/yang/log/log-28562608.out ../submitted
+
+mv /private/home/beidic/yang/log/log-28562607.err ../submitted
+mv /private/home/beidic/yang/log/log-28562607.out ../submitted
+
