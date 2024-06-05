a4b778692358ad9eda6721386bc480c4a479f022
Beidi Chen <beidic@fb.com>
2024-06-04 20:15:05 -0700

    submit jobs

---
+++ submitted/bringbackerrorfiles2.sh
@@ -0,0 +1,21 @@
+# cp /private/home/beidic/yang/log/log-28573693.err ../submitted 
+# cp /private/home/beidic/yang/log/log-28573693.out ../submitted 
+
+# cp /private/home/beidic/yang/log/log-28562800.err ../submitted 
+# cp /private/home/beidic/yang/log/log-28562800.out ../submitted 
+
+# cp /private/home/beidic/yang/log/log-28573113.err ../submitted 
+# cp /private/home/beidic/yang/log/log-28573113.out ../submitted 
+
+# cp /private/home/beidic/yang/log/log-28573114.err ../submitted 
+# cp /private/home/beidic/yang/log/log-28573114.out ../submitted 
+
+# cp /private/home/beidic/yang/log/log-28572752.err ../submitted 
+# cp /private/home/beidic/yang/log/log-28572752.out ../submitted 
+
+cp /private/home/beidic/yang/log/log-28608660.err ../submitted 
+cp /private/home/beidic/yang/log/log-28608660.out ../submitted 
+
+cp /private/home/beidic/yang/log/log-28608661.err ../submitted
+cp /private/home/beidic/yang/log/log-28608661.out ../submitted 
+
